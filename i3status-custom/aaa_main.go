0d9e2b8408c5e1ff5928304e33d70aad7180dc13
Patrick Gaskin <patrick@pgaskin.net>
2026-01-17 15:54:33 -0500

    i3status-custom: Merge configs by machine-id

--- i3status-custom/aaa_main.go
+++ i3status-custom/aaa_main.go
@@ -15,6 +15,8 @@
 package main
 
 import (
+	"os"
+	"slices"
 	"strings"
 	"time"
 
@@ -29,164 +31,229 @@ import (
 		status_command exec /tmp/i3status-custom
 		font pango:DejaVu Sans Mono 8, Font Awesome 5 Pro Regular 8
 	}
+
 	go build -o /tmp/i3status-custom .
 	pkill -f bar_id=test
 	i3bar --bar_id=test --verbose
 */
 
+/*
+	command = "i3status-custom"
+	background = "#00000000"
+	color = "#ccccccee"
+	font = "Font Awesome 5 Pro Regular 8, Noto Sans Mono 8"
+	height = 18
+	layer = "background"
+	separator = "#777777cc"
+	separator_width = 1.0
+
+	go build -o /tmp/i3status-custom .
+	i3bar-river --config config
+*/
+
 func main() {
-	barlib.Main(time.Second/4,
-		// TODO: generic mpris?
-		CMUS{},
-		PulseAudio{
-			ShowSink:   true,
-			ShowSource: true,
-		},
-		Backlight{
-			Interval:  time.Second * 5,
-			Subsystem: "backlight",
-			Name:      "amdgpu_bl1",
-		},
-		Redshift{
-			Latitude:         44.5,
-			Longitude:        -76.5,
-			ElevationDay:     3,  // 3 degrees above the horizon
-			ElevationNight:   -6, // civil twilight
-			TemperatureDay:   6500,
-			TemperatureNight: 3000,
-		},
-		XRandR{},
-		DDC{
-			Interval:   time.Minute,
-			ID:         "ACRE70C-A55C5042",
-			HideIfGone: true,
-			Blind:      true, // read seems to be broken (though windows works somehow)
-			Brightness: true,
-			Contrast:   true,
-			Presets: [][2]uint16{
-				{0, 0},
-				{10, 10},
-				{15, 20},
-				{25, 35},
-				{35, 35},
-				{45, 35},
-				{50, 50},
-				{60, 50},
-				{80, 50},
-				{100, 50},
-				{100, 75},
-				{100, 100},
-			},
-		},
-		DDC{
-			Interval:   time.Minute,
-			ID:         "ACRE70C-00000000", //"ACRE70C-9C5E5042", (the Cable Matters 201376-BLK is nice but it doesn't pass the edid serial properly)
-			HideIfGone: true,
-			Blind:      true, // read seems to be broken (though windows works somehow)
-			Brightness: true,
-			Contrast:   true,
-			Presets: [][2]uint16{
-				{0, 0},
-				{10, 10},
-				{15, 20},
-				{25, 35},
-				{35, 35},
-				{45, 35},
-				{50, 50},
-				{60, 50},
-				{80, 50},
-				{100, 50},
-				{100, 75},
-				{100, 100},
-			},
-		},
-		DDC{
-			Interval:   time.Minute,
-			ID:         "ACR2406-F2179101",
-			HideIfGone: true,
-			Brightness: true,
-			Contrast:   true,
-			Presets: [][2]uint16{
-				{0, 0},
-				{10, 10},
-				{45, 35},
-				{60, 60},
-				{100, 80},
-			},
-		},
-		Battery{
-			Name: "BAT0",
-		},
-		BluezDevice{
-			Label:   "\uf025",
-			Adapter: "hci0",
-			Name:    "dev_00_1B_66_10_CA_67",
-		},
-		BluezDevice{
-			Label:   "\uf025",
-			Adapter: "hci0",
-			Name:    "dev_74_F8_DB_95_10_72",
-		},
-		BluezDevice{
-			Label:   "\uf58f",
-			Adapter: "hci0",
-			Name:    "dev_F0_AE_66_B2_4E_95",
-		},
-		Interfaces{
-			Interval: time.Second * 5,
-			Filter: func(iface string) bool {
-				switch {
-				case strings.HasPrefix(iface, "wg"):
-				case strings.HasPrefix(iface, "en"):
-				case strings.HasPrefix(iface, "wl"):
-				default:
-					return false
-				}
-				return true
-			},
-			Icon: func(iface string) rune {
-				switch {
-				case strings.HasPrefix(iface, "wg"):
-					return '\uf30d'
-				case strings.HasPrefix(iface, "en"):
-					return '\uf6ff'
-				case strings.HasPrefix(iface, "wl"):
-					return '\uf1eb'
-				default:
-					return 0
-				}
-			},
-		},
-		WiFi{
-			Interval:       time.Second * 5,
-			Threshold:      -70,
-			ThresholdColor: 0xFFFF00FF,
-		},
-		Disk{
-			Interval:       time.Second * 5,
-			Threshold:      10 * 1024 * 1024 * 1024,
-			ThresholdColor: 0xFFFF00FF,
-			Mountpoint:     "/home",
-		},
-		CPU{
-			Interval: time.Second * 3,
-		},
-		Temperature{
-			Interval: time.Second * 3,
-			Chip:     "k10temp",
-			Index:    1,
-		},
-		Memory{
-			Interval:       time.Second * 3,
-			Threshold:      1 * 1024 * 1024 * 1024,
-			ThresholdColor: 0xFFFF00FF,
-		},
-		Time{
-			LayoutFull:  "Mon 01/02 15:04:05",
-			LayoutShort: "15:04",
-			Interval:    time.Second,
-			Color:       0x87CEEBFF,
-		},
-		Dunst{},
+	var mid string
+	if buf, err := os.ReadFile("/etc/machine-id"); err == nil {
+		mid = string(buf)
+	}
+
+	const (
+		p1 = "5a" // main laptop (p16s g2 amd 21k9 7840u) (x11 i3wm)
+		s1 = "44" // old laptop (x395 amd 20nl 3500u) (wayland niri)
+		s2 = "2c" // secondary laptop (t14s g6 intel 21qx 258v) (wayland niri)
+		d1 = "22" // desktop (amd) (x11 i3wm, but mostly used headless)
 	)
+
+	var (
+		mods []barlib.Module
+
+		is = func(mids ...string) bool {
+			return slices.ContainsFunc(mids, func(x string) bool {
+				return strings.HasPrefix(mid, x)
+			})
+		}
+		add = func(mod barlib.Module, mids ...string) {
+			if len(mids) == 0 || is(mids...) {
+				mods = append(mods, mod)
+			}
+		}
+	)
+
+	add(CMUS{}, p1, s1, s2) // TODO: generic mpris?
+
+	add(PulseAudio{
+		ShowSink:   true,
+		ShowSource: true,
+	}, p1, s1, s2)
+
+	add(Backlight{
+		Interval:  time.Second * 5,
+		Subsystem: "backlight",
+		Name:      "amdgpu_bl1",
+	}, p1, s1)
+
+	add(Backlight{
+		Interval:  time.Second * 5,
+		Subsystem: "backlight",
+		Name:      "intel_backlight",
+	}, s2)
+
+	add(Redshift{
+		Latitude:         44.5,
+		Longitude:        -76.5,
+		ElevationDay:     3,  // 3 degrees above the horizon
+		ElevationNight:   -6, // civil twilight
+		TemperatureDay:   6500,
+		TemperatureNight: 3000,
+	})
+
+	add(XRandR{}, p1)
+
+	add(DDC{
+		Interval:   time.Minute,
+		ID:         "ACRE70C-A55C5042",
+		HideIfGone: true,
+		Blind:      true, // read seems to be broken (though windows works somehow)
+		Brightness: true,
+		Contrast:   true,
+		Presets: [][2]uint16{
+			{0, 0},
+			{10, 10},
+			{15, 20},
+			{25, 35},
+			{35, 35},
+			{45, 35},
+			{50, 50},
+			{60, 50},
+			{80, 50},
+			{100, 50},
+			{100, 75},
+			{100, 100},
+		},
+	}, p1)
+
+	add(DDC{
+		Interval:   time.Minute,
+		ID:         "ACRE70C-00000000", //"ACRE70C-9C5E5042", (the Cable Matters 201376-BLK is nice but it doesn't pass the edid serial properly)
+		HideIfGone: true,
+		Blind:      true, // read seems to be broken (though windows works somehow)
+		Brightness: true,
+		Contrast:   true,
+		Presets: [][2]uint16{
+			{0, 0},
+			{10, 10},
+			{15, 20},
+			{25, 35},
+			{35, 35},
+			{45, 35},
+			{50, 50},
+			{60, 50},
+			{80, 50},
+			{100, 50},
+			{100, 75},
+			{100, 100},
+		},
+	}, p1)
+
+	add(DDC{
+		Interval:   time.Minute,
+		ID:         "ACR2406-F2179101",
+		HideIfGone: true,
+		Brightness: true,
+		Contrast:   true,
+		Presets: [][2]uint16{
+			{0, 0},
+			{10, 10},
+			{45, 35},
+			{60, 60},
+			{100, 80},
+		},
+	}, p1)
+
+	add(Battery{
+		Name: "BAT0",
+	}, p1, s1, s2)
+
+	add(BluezDevice{
+		Label:   "\uf025",
+		Adapter: "hci0",
+		Name:    "dev_00_1B_66_10_CA_67",
+	}, p1)
+
+	add(BluezDevice{
+		Label:   "\uf025",
+		Adapter: "hci0",
+		Name:    "dev_74_F8_DB_95_10_72",
+	}, p1)
+
+	add(BluezDevice{
+		Label:   "\uf58f",
+		Adapter: "hci0",
+		Name:    "dev_F0_AE_66_B2_4E_95",
+	}, p1)
+
+	add(Interfaces{
+		Interval: time.Second * 5,
+		Filter: func(iface string) bool {
+			switch {
+			case strings.HasPrefix(iface, "wg"):
+			case strings.HasPrefix(iface, "en"):
+			case strings.HasPrefix(iface, "wl"):
+			default:
+				return false
+			}
+			return true
+		},
+		Icon: func(iface string) rune {
+			switch {
+			case strings.HasPrefix(iface, "wg"):
+				return '\uf30d'
+			case strings.HasPrefix(iface, "en"):
+				return '\uf6ff'
+			case strings.HasPrefix(iface, "wl"):
+				return '\uf1eb'
+			default:
+				return 0
+			}
+		},
+	})
+
+	add(WiFi{
+		Interval:       time.Second * 5,
+		Threshold:      -70,
+		ThresholdColor: 0xFFFF00FF,
+	}, p1, s1, s2)
+
+	add(CPU{
+		Interval: time.Second * 3,
+	})
+
+	add(Temperature{
+		Interval: time.Second * 3,
+		Chip:     "k10temp",
+		Index:    1,
+	}, p1, s1, d1)
+
+	add(Temperature{
+		Interval: time.Second * 3,
+		Chip:     "thinkpad",
+		Index:    1,
+	}, s2)
+
+	add(Memory{
+		Interval:       time.Second * 3,
+		Threshold:      1 * 1024 * 1024 * 1024,
+		ThresholdColor: 0xFFFF00FF,
+	})
+
+	add(Time{
+		LayoutFull:  "Mon 01/02 15:04:05",
+		LayoutShort: "15:04",
+		Interval:    time.Second,
+		Color:       0x87CEEBFF,
+	})
+
+	add(Dunst{}, p1, d1)
+
+	barlib.Main(time.Second/4, mods...)
 }
